e7fe782c9a8d5f49e7b96fb6a2f86b42632c2c65
Álvaro Álvarez <aalvarez.contact@icloud.com>
2024-04-18 13:46:21 -0500

    Almost Done
    Final fixes, just missing some translations.

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,25 @@ const toggleText = document.getElementById('toggle-text');
 
 //Colors
 const toggleColors = document.getElementById('toggle-colors');
-
 const rootStyles = document.documentElement.style;
+
+//Languages
+const flagsElement = document.getElementById('flags');
+const textsToChange = document.querySelectorAll("[data-section]");
+const changeLanguage = async language => {
+    const requestJson = await fetch(`./languages/${language}.json`);
+    const texts = await requestJson.json();
+
+    for (const textToChange of textsToChange) {
+        const section = textToChange.dataset.section;
+        const value = textToChange.dataset.value;
+
+        textToChange.innerHTML = texts[section][value];
+    }
+}
+
+flagsElement.addEventListener('click', (e) => changeLanguage(e.target.parentElement.dataset.language));
+
 
 //Theme listener
 toggleTheme.addEventListener('click', () => {
